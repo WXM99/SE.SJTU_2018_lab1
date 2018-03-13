2ade7b232cff81e6266d033006003638661a02ee
WXM99 <wxmxl@hotmail.com>
2018-03-13 23:04:19 +0800

    Update Lab1_dyad_plus.cpp

--- Lab1_dyad_plus.cpp
+++ Lab1_dyad_plus.cpp
@@ -8,19 +8,27 @@ using namespace std;
 inline void keep_wondow_open(){char ch; cin>>ch;}
 
 int main(){
-    cout<<""<<endl;
+    cout<<"expression: "<<endl;
     double lval = 0;
     double rval = 0;
-    char op;
-    double res;
-    cin>>lval>>op>>rval; 
-
-    if(op == '+'){
-        res = lval + rval;
+    cin>>lval;
+    if(!cin){
+        error("enter numbers first");
     }
-    else if(op == '-'){
-        res = lval - rval;
+    for(char op; cin>>op; ){
+        
+        if(op != ';'){
+            cin>>rval;
+        }
+        switch(op){
+            case'+': lval += rval; break;
+            case'-': lval -= rval; break;
+            case'*': lvak *= rval; break;
+            case'/': lval /= rval; break;
+            default://''
+                cout<<"result: "<<lval<<endl;
+                keep_window_open();
+                return 0;           
+        }
     }
-    cout<<res<<endl;
-    return 0;
 }
